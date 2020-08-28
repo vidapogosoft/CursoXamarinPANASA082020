528b0cae079ee4a42e3e24b812cb24a0a14e45c9
vidapogosoft <vidapogosoft@hotmail.com>
2020-08-27 20:43:54 -0500

    Talleres dia 3
    Ejercicios del dia

---
+++ TallerModulo1/TallerModulo1/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using TallerModulo1.Views;
+
+namespace TallerModulo1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            MainPage = new Views.Menu();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ TallerModulo1/TallerModulo1/Views/Menu.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TallerModulo1.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Menu : MasterDetailPage
+    {
+        public Menu()
+        {
+            InitializeComponent();
+            MyMenu();
+        }
+
+
+        public void MyMenu()
+        {
+            Detail = new NavigationPage(new Home());
+
+            List<MenuLateral> menu = new List<MenuLateral>
+            {
+                new MenuLateral { Page = new Home(), MenuTitle = "Home" },
+                new MenuLateral { Page = new Contactenos(), MenuTitle = "Contactenos" }
+
+            };
+
+            ListMenu.ItemsSource = menu;
+
+
+        }
+
+        public class MenuLateral
+        { 
+            public string MenuTitle { get; set; }
+
+            public Page Page { get; set; }
+
+        }
+
+
+    }
+}
