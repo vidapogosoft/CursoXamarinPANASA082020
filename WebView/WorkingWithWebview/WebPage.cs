e26db58da7e1332e39c910bc99dc886aca67be9e
vidapogosoft <vidapogosoft@hotmail.com>
2020-09-18 17:34:55 -0500

    talleres dia del cierre
    ultimos talleres realizados

---
+++ WebView/WorkingWithWebview/WebPage.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+
+namespace WorkingWithWebview
+{
+    public class WebPage : ContentPage
+    {
+        public WebPage()
+        {
+            var browser = new WebView();
+            browser.Source = "http://xamarin.com";
+            Content = browser;
+        }
+    }
+}
+
